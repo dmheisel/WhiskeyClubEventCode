6a5066fa7454756da3bbaf62980cf955aa1c9cf5
dmheisel <david.heisel@granicus.com>
2019-12-21 02:20:40 -0600

    event options, guest list, better comments.

--- createWhiskeyClubEvent.js
+++ createWhiskeyClubEvent.js
@@ -1,9 +1,11 @@
 //creates the event!
 function createWhiskeyEvent() {
 	var eventRecurrence = buildRecurrenceRule();
-	var startDate = getDates(2020)[0]; // need this to set the start and end times on the event
+	var guestList = guestList();
+	var startDate = getDates(2020)[0]; // need this to set the start and end times on the event because reasons
 	var startTime = new Date(startDate.setHours(18));
 	var endTime = new Date(startDate.setHours(22));
+
 	var whiskeyEvent = CalendarApp.getDefaultCalendar()
 		.createEventSeries(
 			"Polybius Whiskey Club Meeting!",
@@ -12,8 +14,10 @@ function createWhiskeyEvent() {
 			eventRecurrence,
 			{
 				description:
-					"Let's get together for some whiskey, scotch, whisky, bourbon, rye...but most importantly, good times with good friends! \n\nIn the spirit of software development, it feels necessary that this meeting has an odd component.  Therefor, we are meeting on the second-to-last Thursday of every month.  Default location is Merlin's Rest.  \n\nPlease be sure to RSVP within a week prior to the event, so I can call ahead and get a table for us/check for conflicts!  If anyone's interested, you can find the code I used to make this event on my Github!",
-				location: "Merlins Rest"
+					"Let's get together for some whiskey, scotch, whisky, bourbon, rye...but most importantly, good times with good friends! \n\nIn the spirit of software development, it feels necessary that this meeting's schedule has an odd component.  Therefor, we are meeting on the second-to-last Thursday of every month.  Default location is Merlin's Rest -- so far a pretty suitable place for code-wizards but subject to change with future discussions.  \n\nPlease be sure to RSVP and notify of any +1s by the preceding Monday, so I can call ahead and get a table for us/check for conflicts.  \n\n And if  anyone's interested, you can find the code I used to make this event on my Github! (turns out, Google Calendars doesn't natively support second-to-last Thursday scheduling, so this was made in Google Apps Script.)",
+				location: "Merlins Rest",
+				sendInvites: true,
+				guestList: guestList
 			}
 		)
 		.setAnyoneCanAddSelf(true)
@@ -22,7 +26,7 @@ function createWhiskeyEvent() {
 		.setGuestsCanModify(false)
 		.addEmailReminder(8640); // email reminder 6 days before the event
 }
-//grabs the list of attendees from our last gathering in December 2019
+//grabs the list of attendees from our last gathering in December 2019 -- may update to pull from slack somehow?
 function getAttendees() {
 	var date = new Date("December 19, 2019");
 	var guestList = CalendarApp.getDefaultCalendar() // find my calendar, get the second event for the day, get the guest list, and get everyone's emails.
@@ -32,11 +36,11 @@ function getAttendees() {
 			return guest.getEmail();
 		})
 		.join();
-	Logger.log(guestList);
+	// Logger.log(guestList);
 	return guestList;
 }
 
-//a function to figure out what the second to last thursday was in the specified month/year
+//figure out what the second to last thursday was in the specified month/year
 function getSecondToLastThursday(month, year) {
 	var daysInMonth = new Date(year, month, 0).getDate(); // get total number of days in the month (gets the date of the last day of the month)
 	var counter = 0;
@@ -53,7 +57,7 @@ function getSecondToLastThursday(month, year) {
 		}
 	}
 }
-//function to collect all the dates of second to last thursdays for one year.
+//collect all the dates of second to last thursdays for one year.
 function getDates(year) {
 	var dates = [];
 	for (var i = 1; i <= 12; i++) {
@@ -61,7 +65,7 @@ function getDates(year) {
 	}
 	return dates;
 }
-//function to build the recurrenceRule for the CalendarApp Event Series, using the dates gathered in the getDates function.
+//build the recurrenceRule for the CalendarApp Event Series, using the dates gathered in the getDates function.
 function buildRecurrenceRule() {
 	var recurrenceRule = CalendarApp.newRecurrence();
 	var dates = getDates(2020);
